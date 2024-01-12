0ea147207efee66bfd641d998ee57337476719ef
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-01-12 13:50:06 -0500

    Minimal sdpa scaling example

---
+++ scripts/sdpa_example.py
@@ -0,0 +1,18 @@
+import torch
+import time
+import torch.nn.functional as f
+
+def _time():
+    torch.cuda.synchronize()
+    return time.time()
+
+bsizes = [1,2,4,8,16]
+times = {b:0 for b in bsizes}
+for bsize in bsizes:
+    for _ in range(100):
+        inp = torch.randn(bsize, 40, 2048, 128).cuda()
+        _start = _time()
+        f.scaled_dot_product_attention(inp, inp, inp, is_causal=True)
+        times[bsize] += _time() - _start
+for b in times:
+    print("Bsize {b}, time:"+"{:.2f}".format(times[b]))
