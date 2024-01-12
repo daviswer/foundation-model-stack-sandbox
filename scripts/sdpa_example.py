70ddb389868acbd11f1e8c1ea352546efeb5870c
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-01-12 13:59:15 -0500

    Minimal sdpa scaling example pt2

--- scripts/sdpa_example.py
+++ scripts/sdpa_example.py
@@ -9,6 +9,7 @@ def _time():
 bsizes = [1,2,4,8,16]
 times = {b:0 for b in bsizes}
 for bsize in bsizes:
+    print("Benchmarking bsize {bsize}...")
     for _ in range(100):
         inp = torch.randn(bsize, 40, 2048, 128).cuda()
         _start = _time()
