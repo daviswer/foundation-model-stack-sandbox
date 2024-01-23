c828eafd7fba2dd9ee992fe1a90a83a494e9fb3f
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-01-23 14:41:45 -0500

    Rollforward to bench 2: compile specu flatting

--- scripts/benchmark_speculative_inference.py
+++ scripts/benchmark_speculative_inference.py
@@ -157,7 +157,7 @@ for k in [1,2,4,8,16,32]:
             inp = [torch.IntTensor(line).cuda() for line in seqs]
             with torch.no_grad():
                 start_time = time.time()
-                out, nsteps, generation_time, times = generate(
+                out, nsteps, generation_time, times = speculative_generate(
                     model,
                     inp,
 
