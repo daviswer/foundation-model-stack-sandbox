2b2600d0bcaf82ac159acd058a1b6ba84356b273
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-01-23 22:50:33 -0500

    Run flat and noflat in the same benchmark for consistency

--- scripts/benchmark_speculative_inference.py
+++ scripts/benchmark_speculative_inference.py
@@ -73,11 +73,6 @@ parser.add_argument(
     action="store_true",
     help="Call torch compile on base model",
 )
-parser.add_argument(
-    "--noflat",
-    action="store_true",
-    help="Disable efficient tree attention",
-)
 parser.add_argument("--device_type", type=str, default="cuda")
 
 args = parser.parse_args()
@@ -151,56 +146,57 @@ for k in [1,2,4,8,16,32]:
     steps = {}
     outs = []
     for bsize in [1, 2, 4]:
-        alltimes = {}
-        ntok = 0
-        torch.cuda.empty_cache()
-        torch.cuda.reset_peak_memory_stats()
-        for j in range(30): #len(data) // bsize):
-            seqs = data[j * bsize : j * bsize + bsize]
-            max_seq = max(len(line) for line in seqs)
-            inp = [torch.IntTensor(line).cuda() for line in seqs]
-            with torch.no_grad():
-                start_time = time.time()
-                out, nsteps, generation_time, times = speculative_generate(
-                    model,
-                    inp,
-                    
-                    test,
-                    new_tokens=100,
-                    threshes=[6,3,2],
-                    flatting=not args.noflat,
-                    # max_new_tokens=30,
-                    # use_cache=True,
-                    # do_sample=False,
-                    # expand = True,
-                    
-                    max_seq_len=4096,
-                    top_k=k,
-                    kv_cache_manager=kv_cache,
-                )
-            end_time = time.time()
-            total_time = end_time - start_time
-            ntok += sum([len(line) for line in out]) - sum([len(line) for line in inp])
-            # if k == 5:
-            #     outs += [line.squeeze().tolist() for line in out]
+        for flatting in [True, False]:
+            alltimes = {}
+            ntok = 0
+            torch.cuda.empty_cache()
+            torch.cuda.reset_peak_memory_stats()
+            for j in range(30): #len(data) // bsize):
+                seqs = data[j * bsize : j * bsize + bsize]
+                max_seq = max(len(line) for line in seqs)
+                inp = [torch.IntTensor(line).cuda() for line in seqs]
+                with torch.no_grad():
+                    start_time = time.time()
+                    out, nsteps, generation_time, times = speculative_generate(
+                        model,
+                        inp,
+                        
+                        test,
+                        new_tokens=100,
+                        threshes=[6,3,2],
+                        flatting=flatting,
+                        # max_new_tokens=30,
+                        # use_cache=True,
+                        # do_sample=False,
+                        # expand = True,
+                        
+                        max_seq_len=4096,
+                        top_k=k,
+                        kv_cache_manager=kv_cache,
+                    )
+                end_time = time.time()
+                total_time = end_time - start_time
+                ntok += sum([len(line) for line in out]) - sum([len(line) for line in inp])
+                # if k == 5:
+                #     outs += [line.squeeze().tolist() for line in out]
 
-            # for i in range(bsize):
-            #     num_generated = len(out[i]) - len(inp[i])
-            #     # print(f"Ex {j*bsize+i}, topk={k}: {num_generated} tokens in {nsteps} steps.")
-            #     # print(f"--- avg per token: {total_time / len(out[i])}, avg per new token: {generation_time / num_generated}")
-            #     steps[k].append([nsteps * 100 / num_generated, total_time / len(out[i]), generation_time / num_generated])
-            
-            for field in times:
-                if field not in alltimes:
-                    alltimes[field] = 0
-                else:
-                    alltimes[field] += times[field]
-        print()
-        print("bsize =",bsize,"k =",k)
-        for field in alltimes:
-            print(field, "{:.2f}".format(alltimes[field]))
-        print("Ntok:", ntok)
-        # print(torch.cuda.max_memory_allocated()//1000000/1000)
+                # for i in range(bsize):
+                #     num_generated = len(out[i]) - len(inp[i])
+                #     # print(f"Ex {j*bsize+i}, topk={k}: {num_generated} tokens in {nsteps} steps.")
+                #     # print(f"--- avg per token: {total_time / len(out[i])}, avg per new token: {generation_time / num_generated}")
+                #     steps[k].append([nsteps * 100 / num_generated, total_time / len(out[i]), generation_time / num_generated])
+                
+                for field in times:
+                    if field not in alltimes:
+                        alltimes[field] = 0
+                    else:
+                        alltimes[field] += times[field]
+            print()
+            print("bsize =",bsize,"k =",k,"flatting =",flatting)
+            for field in alltimes:
+                print(field, "{:.2f}".format(alltimes[field]))
+            print("Ntok:", ntok)
+            # print(torch.cuda.max_memory_allocated()//1000000/1000)
         
 
 # if len(args.output_path) > 0:
