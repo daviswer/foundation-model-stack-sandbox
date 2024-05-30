f9a5d7759ebc417929f99aa69f838e8c69801b6a
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-05-30 14:06:52 -0400

    Passing rank/worldsize stuff

--- scripts/eval_harness.py
+++ scripts/eval_harness.py
@@ -149,7 +149,13 @@ if args.compile:
     model = torch.compile(model, mode=args.compile_mode)
 
 
-lm_obj = evaluation.FMSEvalHarnessLM(model=model, tokenizer=tokenizer, device=device, local_rank=local_rank)
+lm_obj = evaluation.FMSEvalHarnessLM(
+    model=model, 
+    tokenizer=tokenizer, 
+    device=device, 
+    rank=local_rank, 
+    world_size=world_size,
+)
 
 lm_eval.tasks.initialize_tasks()
 
