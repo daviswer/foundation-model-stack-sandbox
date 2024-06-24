fe8bd0149e39def9d1bbd9d2967fe0277d7da978
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-06-24 17:29:39 -0400

    bkwd b block size 32

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -491,7 +491,7 @@ class MultiHeadAttention(nn.Module):
                 padded_indices_per_block,
                 value_block_mapping,
                 total_padded_indices,
-            ) = invert_mapping_gpu(self.plan[-1], keys.size(1), 64)
+            ) = invert_mapping_gpu(self.plan[-1], keys.size(1), 32)
             self.Mp = padded_indices_per_block
             self.Mv = value_block_mapping
             self.Mt = total_padded_indices
