4f56adef55be364998f40403b3c9d86d09c40cac
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-07-05 17:07:05 -0400

    Revert extra weighting + detach

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -394,8 +394,6 @@ class MultiHeadAttention(nn.Module):
             self.matscan = MatScan.apply
 
         self.weighted = True
-        if self.weighted:
-            self.w = nn.Linear(self.emb_kq_per_head, 1, False)
 
     def reset_parameters(self):
         for m in self.modules():
@@ -405,7 +403,6 @@ class MultiHeadAttention(nn.Module):
                     m.bias.data.zero_()
             elif isinstance(m, LayerNormParameterized) or isinstance(m, QKV):
                 m.reset_parameters()
-        self.w.weight.data.zero_()
 
     def to_tp(self, group: ProcessGroup) -> "TPMultiHeadAttention":
         return TPMultiHeadAttention.import_module(self, group)
@@ -554,8 +551,7 @@ class MultiHeadAttention(nn.Module):
         # k/v: b l h d
         w = None
         if self.weighted:
-            w = queries.detach().mul(keys.unsqueeze(3).detach()).mul(self.w.weight.add(1)).sum(-1).logsumexp(-1, True)
-            # w = queries.matmul(keys.unsqueeze(-1)).squeeze(-1).logsumexp(-1, True)  # b l h 1
+            w = queries.matmul(keys.unsqueeze(-1)).squeeze(-1).logsumexp(-1, True)  # b l h 1
         if not self.scan_impl:
             # keys = keys.view(batch_size, kv_len, -1)
             keys = self.scan(keys, self.plan, None, 4, w)  # b l h d 64
