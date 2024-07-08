d8e097b964c19ee50a7a8f17e316518a920b3994
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-07-08 19:52:19 -0400

    Move exp to very end

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -340,11 +340,13 @@ class MultiHeadAttention(nn.Module):
         # k_neg_v = v_sep.unsqueeze(-2).sub(k_proj.transpose(3,4)).logcumsumexp(1)  # b l h d d+d
         
         # Perform querying
-        kv_pos = k_pos_v.unsqueeze(3).sub(qk_denom.unsqueeze(-1).unsqueeze(-1)).exp()  # b l h e d d+d
+        kv_pos = k_pos_v.unsqueeze(3).sub(qk_denom.unsqueeze(-1).unsqueeze(-1))#.exp()  # b l h e d d+d
         # kv_neg = k_neg_v.unsqueeze(3).sub(qk_denom.unsqueeze(-1).unsqueeze(-1)).exp()  # b l h e d d+d
-        qkv_pos = q_proj.exp().unsqueeze(-1).mul(kv_pos).sum(4)  # b l h e d+d
+        # qkv_pos = q_proj.exp().unsqueeze(-1).mul(kv_pos).sum(4)  # b l h e d+d
+        qkv_pos = q_proj.unsqueeze(-1).add(kv_pos).logsumexp(4)  # b l h e d+d
         # qkv_neg = q_proj.neg().exp().unsqueeze(-1).mul(kv_neg).sum(4)  # b l h e d+d
         # qkv = qkv_pos.add(qkv_neg).view(batch_size, q_len, self.nheads, 2, self.emb_v_per_head)  # b l he 2 d
+        qkv = qkv.exp()
         qkv = qkv_pos.view(batch_size, q_len, self.nheads, 2, self.emb_v_per_head)  # b l he 2 d
         qkv = qkv[:,:,:,0] - qkv[:,:,:,1]  # b l he d
         
