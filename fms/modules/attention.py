1092da04e2c0aeda97d39ad9a49fbf9b64953435
Davis Wertheimer <dww78@cornell.edu>
2024-07-12 17:01:01 -0400

    Vanilla attn layers

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -12,67 +12,8 @@ from fms.distributed.tensorparallel import (
     copy_to_tensor_model_parallel_region,
     reduce_from_tensor_model_parallel_region,
 )
-from fms.modules.layernorm import LayerNormParameterized
 from fms.modules.positions import PositionEncoder
 from fms.modules.tp import TPModule
-from fms.triton.index_linear import IndLinear, IndLinearTransposed, invert_mapping_gpu
-
-
-def get_scan_plan(x, fmap, h):
-    # x: b n d
-    # plan: for each level, which entries to avg from previous level ([l] n' 2)
-    # inds: which level and entry to pull from in populating heads (n h 2) -> (n h)
-    b, n, d = x.size()
-    # Form ruler-tick progression sequence
-    levels = sum(
-        [
-            torch.arange(n, device=x.device)
-            .remainder(2**i)
-            .sub(2**i - 1)
-            .sign()
-            .add(1)
-            for i in range(n.bit_length())
-        ]
-    ).roll(1, 0)
-    plan = [
-        torch.zeros(0, 2, device=x.device, dtype=torch.int)
-        for _ in range(len(fmap) + 2)
-    ]  # [l] 0 2
-    plan[1] = (
-        torch.arange(x.size(1) + 1, device=x.device, dtype=torch.int)
-        .unsqueeze(1)
-        .expand(-1, 2)
-    )
-    inds = torch.zeros(n, h, 2, device=x.device, dtype=torch.long)  # n h 2
-    inds[:, 0, 1] = torch.arange(n, device=inds.device, dtype=inds.dtype) + 1
-    inds[:, :, 0] = 1
-    for i in range(1, n):
-        m = fmap.get(levels[i].item(), h)
-        inds[i, 1:m] = inds[i - 1, : m - 1]
-        if m < h:
-            inds[i, m + 1 :] = inds[i - 1, m + 1 :]
-            prev = inds[i - 1, m - 1 : m + 1].flip([0])  # 2 2
-            assert prev[0, 0] == min(levels[i], len(fmap) + 1) or prev[0, 1] == 0, (
-                levels[i],
-                prev[0, 0],
-            )
-            assert prev[1, 0] == min(levels[i], len(fmap) + 1) or prev[1, 1] == 0, (
-                levels[i],
-                prev[1, 0],
-            )
-            level = plan[levels[i] + 1]
-            inds[i, m, 0] = levels[i] + 1
-            inds[i, m, 1] = level.size(0)
-            plan[levels[i] + 1] = torch.cat(
-                [plan[levels[i] + 1], prev[:, 1][None]], dim=0
-            )
-    # Flatten inds (indexing into flattened plan/cache) (n h)
-    ls = [p.size(0) for p in plan]
-    ls = [0] + ls[:-1]
-    offset = torch.tensor(ls, device=inds.device).cumsum(0)
-    offset = offset[inds[:, :, 0]]
-    inds = offset + inds[:, :, 1]
-    return plan + [inds]
 
 
 class QKV(nn.Module, metaclass=abc.ABCMeta):
@@ -227,6 +168,28 @@ class FusedQKV(QKV):
             bias=self.use_bias,
         )
 
+    def unfuse_weights(self):
+        result = UnfusedQKV(
+            self.emb_dim,
+            self.nheads,
+            self.kvheads,
+            self.emb_kq_per_head,
+            self.emb_v_per_head,
+            self.use_bias,
+        ).to(self.qkv_fused.weight.device)
+        query, key, value = torch.split(self.qkv_fused.weight, self.splits, dim=0)
+        result.query.weight.copy_(query)
+        result.key.weight.copy_(key)
+        result.value.weight.copy_(value)
+        if self.use_bias:
+            query_bias, key_bias, value_bias = torch.split(
+                self.qkv_fused.bias, self.splits, dim=0
+            )
+            result.query.bias.copy_(query_bias)
+            result.key.bias.copy_(key_bias)
+            result.value.bias.copy_(value_bias)
+        return result
+
     def reset_parameters(self):
         nn.init.trunc_normal_(self.qkv_fused.weight, mean=0.0, std=0.02)
         if self.use_bias:
@@ -301,48 +264,12 @@ class MultiHeadAttention(nn.Module):
         if self.p_dropout:
             self.attn_dropout = nn.Dropout(self.p_dropout)
         self.position_encoder = position_encoder
-        # self.ln_k = LayerNormParameterized(
-        #     emb_kq, use_high_precision_pow=True
-        # )
-        # self.ln_v = LayerNormParameterized(emb_v, use_high_precision_pow=True)
-
-        self.inp_len = 0
-        self.cache_len = 0
-        self.plan = None
-
-        # fmap = {8 - i: 64 - (i) ** 2 for i in range(8)}
-        # fmap.pop(8)
-        # fmap.pop(7)
-        # fmap = {
-        #     1:7,
-        #     2:13,
-        #     3:18,
-        #     4:22,
-        #     5:25,
-        #     6:27,
-        #     7:29
-        # }
-        fmap = {
-            1:26,
-            2:50,
-            3:71,
-            4:89,
-            5:104,
-            6:116,
-            7:124
-        }
-        self.fmap = fmap
-        self.cache_size = 128
-
-        self.weighted = True
-        # if self.weighted:
-        #     self.w = nn.Linear(self.emb_dim, self.kvheads, bias=False)
-
-        self.indlinear = IndLinear.apply
-        self.indlineart = IndLinearTransposed.apply
-        self.Mv = None
-        self.Mp = None
-        self.Mt = None
+        # Avoiding graph breaks
+        self.previous_flash: bool = torch.backends.cuda.flash_sdp_enabled()
+        self.previous_mem_efficient: bool = (
+            torch.backends.cuda.mem_efficient_sdp_enabled()
+        )
+        self.previous_math: bool = torch.backends.cuda.math_sdp_enabled()
 
     def reset_parameters(self):
         for m in self.modules():
@@ -350,61 +277,11 @@ class MultiHeadAttention(nn.Module):
                 nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02)
                 if self.use_bias:
                     m.bias.data.zero_()
-            elif isinstance(m, LayerNormParameterized) or isinstance(m, QKV):
+            elif isinstance(m, QKV):
                 m.reset_parameters()
 
     def to_tp(self, group: ProcessGroup) -> "TPMultiHeadAttention":
         return TPMultiHeadAttention.import_module(self, group)
-
-    def scan(self, x, plan, ln, i, w=None):
-        """
-        Takes input x of shape [b n ...] and scan plan, computes recursive sums, and
-        extracts cache values into the dimension specified by i > 1.
-        Final output shape is therefore [b n ... c ...] with c the cache size.
-        Applies specified LN to cache, so LN size should match x.size(-1).
-        """
-        assert i > 1, "Output must maintain batch and seqlen in first two dimensions"
-        s = x.size()
-        weighted = w is not None
-        if weighted:
-            ws = w.size()
-            weights = nn.functional.pad(w.view(s[0], s[1], -1), (0,0,1,0), value=-1000).view(
-                s[0], s[1] + 1, *ws[2:]
-            )
-        inds = plan[-1]  # n h
-        plan = plan[:-1]
-        # Plan and inds are formed, construct cache via recursive sums
-        cache = [None for _ in plan]
-        cache[1] = nn.functional.pad(x.view(s[0], s[1], -1), (0, 0, 1, 0)).view(
-            s[0], s[1] + 1, *s[2:]
-        )  # b n ...
-        for j in range(2, len(cache)):
-            if weighted:
-                weights = weights.index_select(1, plan[j].view(-1)).view(s[0], -1, 2, *ws[2:])
-                weights_ = weights.softmax(dim=2)
-                weights = weights.logsumexp(2)
-            cache[j] = (
-                cache[j - 1]
-                .index_select(1, plan[j].view(-1))
-                .view(s[0], -1, 2, *s[2:])
-            )
-            if weighted:
-                cache[j] = cache[j].mul(weights_).sum(2)
-            else:
-                cache[j] = cache[j].sum(2).div(2**0.5)
-            
-        cache = torch.cat(cache[1:], dim=1)  # b n' ...
-        if ln is not None:
-            cache = ln(cache)
-        return cache
-        # cache = cache.unsqueeze(i).expand(
-        #     *[-1] * i, inds.size(-1), *[-1] * (len(s) - i)
-        # )  # b n' ... h ...
-        # inds = inds.view(
-        #     1, inds.size(0), *[1] * (i - 2), inds.size(1), *[1] * (len(s) - i)
-        # )  # b n 111 h 111
-        # inds = inds.expand(s[0], -1, *s[2:i], -1, *s[i:])  # b n ... h ...
-        # return cache.gather(1, inds)  # b n ... h ...
 
     def forward(
         self,
@@ -440,16 +317,6 @@ class MultiHeadAttention(nn.Module):
         # q, k, v: batch_size x seq_len x emb_dim
         # mask: batch_size x seq_len x seq_len
         batch_size, q_len, _ = q.size()
-        if is_self:
-            k = q
-            v = q
-        kv_len = k.size(1)
-
-        # if kv_len mismatch, build new scan plan
-        if kv_len != self.inp_len:
-            self.inp_len = kv_len
-            with torch.no_grad():
-                self.plan = get_scan_plan(k, self.fmap, self.cache_size)
 
         # if this is self attention, we always recompute
         # cross attention only gets computed when a cache does not exist
@@ -466,7 +333,6 @@ class MultiHeadAttention(nn.Module):
             queries = q_out.view(batch_size, q_len, self.nheads, self.emb_kq_per_head)
             keys = k_out.view(batch_size, q_len, self.kvheads, self.emb_kq_per_head)
             values = v_out.view(batch_size, q_len, self.kvheads, self.emb_v_per_head)
-            # sink = queries.sum(3)  # b l he
 
             # You want to apply rotary embeddings pre-cache
             if self.position_encoder is not None:
@@ -474,30 +340,9 @@ class MultiHeadAttention(nn.Module):
                     queries, keys, position_ids, past_key_value_state, use_cache
                 )
 
-        queries = queries / (self.emb_kq_per_head**0.5)  # b l h d
-        expansion = self.nheads // self.kvheads
-        queries = queries.unflatten(2, (self.kvheads, expansion))  # b l h e d
-        # sink = sink / (self.emb_kq_per_head**0.5)
-
-        # Build telescoping cache
-        # k/v: b l h d
-        w = None
-        if self.weighted:
-            w = queries.matmul(keys.unsqueeze(-1)).squeeze(-1).logsumexp(-1, True)  # b l h 1
-        keys = self.scan(keys, self.plan, None, 4, w)  # b n h d
-        values = self.scan(values, self.plan, None, 3, w)  # b n h d
-
-        # if you built a new scan plan, invert the plan for use by backward kernels
-        if keys.size(1) != self.cache_len:
-            self.cache_len = keys.size(1)
-            (
-                padded_indices_per_block,
-                value_block_mapping,
-                total_padded_indices,
-            ) = invert_mapping_gpu(self.plan[-1], keys.size(1), 16)
-            self.Mp = padded_indices_per_block
-            self.Mv = value_block_mapping
-            self.Mt = total_padded_indices
+        queries = queries.transpose(2, 1)  # / (self.emb_kq_per_head**(1/4))
+        keys = keys.transpose(2, 1)  # / (self.emb_kq_per_head**(1/4))
+        values = values.transpose(2, 1)  # compatible with QK.T
 
         # if you want to use caching and past_key_value_state is not None meaning you have values in your cache
         if (
@@ -512,26 +357,65 @@ class MultiHeadAttention(nn.Module):
                 keys = past_key_value_state[0]
                 values = past_key_value_state[1]
 
-        # b l h e d, b n h d, l c
-        attn = self.indlinear(
-            queries, 
-            keys, 
-            self.plan[-1],
-            self.Mp,
-            self.Mv,
-            self.Mt
-        )  # b l h e c
-        attn = attn.softmax(4)
-        attn = self.indlineart(
-            attn, 
-            values, 
-            self.plan[-1],
-            self.Mp,
-            self.Mv,
-            self.Mt,
-        )  # b l h e d
+        # Merge rel pos bias and mask into single float mask
+        if mask is not None:
+            # Our expected mask format is bs x q_len x k_len, so to make it broadcastable
+            # we need to create the nheads dimension
+            while len(mask.size()) != 4:  # expects bs (x nheads) x q_len x kv_len
+                mask = mask.unsqueeze(1)
 
-        attn = attn.reshape(batch_size, q_len, self.nheads * self.emb_v_per_head)
+        if self.position_encoder is not None:
+            attn_mask: Optional[Tensor] = self.position_encoder.adjusted_mask(
+                mask, queries, keys, past_key_value_state, use_cache
+            )
+        else:
+            attn_mask = mask
+
+        # Expand kv so black-box attn will work
+        expansion = self.nheads // self.kvheads
+        # k/v: b h l d
+        if expansion != 1:
+            keys_e = keys.unsqueeze(2).expand(-1, -1, expansion, -1, -1).flatten(1, 2)
+            values_e = (
+                values.unsqueeze(2).expand(-1, -1, expansion, -1, -1).flatten(1, 2)
+            )
+        else:
+            keys_e = keys
+            values_e = values
+
+        if attn_algorithm:
+            # Pick which fused attn kernels will run.
+            use_flash = attn_algorithm == "flash"
+            use_mem_efficient = attn_algorithm == "mem"
+            use_math = attn_algorithm == "math"
+
+            torch.backends.cuda.enable_flash_sdp(use_flash)
+            torch.backends.cuda.enable_mem_efficient_sdp(use_mem_efficient)
+            torch.backends.cuda.enable_math_sdp(use_math)
+
+        attn = F.scaled_dot_product_attention(
+            queries,
+            keys_e,
+            values_e,
+            attn_mask=attn_mask,
+            dropout_p=self.p_dropout if self.training else 0.0,
+            is_causal=is_causal_mask,
+        )
+
+        if attn_algorithm:
+            torch.backends.cuda.enable_flash_sdp(self.previous_flash)
+            torch.backends.cuda.enable_mem_efficient_sdp(self.previous_mem_efficient)
+            torch.backends.cuda.enable_math_sdp(self.previous_math)
+
+        # attn: bs x seq_len x nheads*emb_v_per_head
+        # attn: b x h x qlen x ds
+        # attn after permute: b x qlen x h x ds
+        # b x qlen x (d)
+        attn = (
+            attn.transpose(2, 1)
+            .contiguous()
+            .view(batch_size, q_len, self.nheads * self.emb_v_per_head)
+        )
         out = self.dense(attn)
 
         # if use_cache=True, we return the hidden_state as well as the kv cache
