a24abb8a2ffc7f6e23d37e4de50dbaec49922b58
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-07-15 16:05:57 -0400

    Fix last transpose

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -503,10 +503,9 @@ class MultiHeadAttention(nn.Module):
         else:
             keys_e = keys.transpose(1,2)
             values_e = values.transpose(1,2)
-        queries = queries.view(batch_size, q_len, self.nheads, self.emb_kq_per_head).transpose(0,1)
+        queries = queries.view(batch_size, q_len, self.nheads, self.emb_kq_per_head).transpose(1,2)
 
         # q/k/v: b h n d
-        print(queries.shape, keys_e.shape, values_e.shape)
         attn = F.scaled_dot_product_attention(queries, keys_e, values_e, self.mask)
         attn = attn.transpose(1,2)  # b l h d
         attn = attn.reshape(batch_size, q_len, self.nheads * self.emb_v_per_head)
