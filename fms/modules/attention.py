034187415696388c1107d7ace432aae87f17c2f6
Davis Wertheimer <dww78@cornell.edu>
2025-11-05 20:31:57 -0500

    Update prints

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -956,8 +956,8 @@ class GatedMultiHeadAttention(nn.Module):
         rank = int(os.environ["RANK"])
         if attn_compute_dict["is_prefill"](**attn_kwargs):
             if rank==0 and verbose:
-                print(queries[0,0,-1,:4], queries.shape)
-                print(keys_compute[0,0,-1,:4])
+                print(queries[0,-1,0,:4], queries.shape)
+                print(keys_compute[0,0,-1,:4], keys_compute.shape)
             attn = attn_compute_dict["compute_prefill"](
                 queries,
                 keys_compute,
