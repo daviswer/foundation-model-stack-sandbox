408393d883a19592356525dee03ff667f251863a
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-11-07 17:04:35 -0500

    Swap back to inject, compile

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -138,6 +138,21 @@ class UniversalAttention(Function):
 
         return dkc,dvc,dxq,dstat_src,dstat_dest
 
+class InjectAux(Function):
+    @staticmethod
+    def forward(affs):
+        aux = affs.exp().gt(.001).to(dtype=affs.dtype).sum()  # *l*l / (l*(l+1)/2)  =  *2l/(l+1)
+        q_len = affs.size(-1)
+        aux = aux * (2 * q_len / (q_len+1) / affs.numel())
+        return affs, aux
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        pass
+    @staticmethod
+    def backward(ctx, g_affs, g_aux):
+        return g_affs+g_aux/g_affs.numel(), None
+    
+inject_aux = torch.compile(InjectAux.apply)
 
 class SMVecMatMul(Function):
     @staticmethod
@@ -604,17 +619,26 @@ class MultiHeadAttention(nn.Module):
             rates = static_src
 
             r = self.nheads // self.kvheads
-            mask, aux = self._gen_affinity_scores(keys, static_src, static_dest, r)  # b h l_q l_k
+            mask = self._gen_affinity_scores(keys, static_src, static_dest, r)  # b h l_q l_k
+
+            mask, aux = inject_aux(mask)
+
+            # affs = mask.view(batch_size, self.kvheads, -1, mask.size(-2), mask.size(-1))[:,:,0].exp()  # b h l l
+            # affsm = affs.mean()
+            # with torch.no_grad():
+            #     aux = affs.gt(.001).to(dtype=affs.dtype).mean()  # *l*l / (l*(l+1)/2)  =  *2l/(l+1)
+            #     aux = aux * (2 * q_len / (q_len+1))
+            # aux = aux.sub(affsm.detach()).add(affsm)
+
             torch.backends.cuda.enable_math_sdp(False)
             attn = F.scaled_dot_product_attention(
                 queries, 
                 torch.repeat_interleave(keys,r,dim=1), 
                 torch.repeat_interleave(values,r,dim=1), 
-                attn_mask=mask,
+                attn_mask=torch.repeat_interleave(mask,r,dim=1),
                 scale=1,
             )  # b h l d
             attn = attn.transpose(1,2).contiguous()  # b l h d
-            
 
             # c = 512
             # b = batch_size
@@ -668,14 +692,8 @@ class MultiHeadAttention(nn.Module):
         affinity = torch.log1p(affinity.clamp(min=0, max=1-1e-6).neg())
         affinity = affinity.triu(1).cumsum(3).to(dtype=k.dtype)
         affinity = affinity.masked_fill(torch.ones_like(affinity, dtype=torch.bool).tril(-1), -1e12).transpose(-1, -2)
-        # Aux
-        affsm = affinity.mean()
-        with torch.no_grad():
-            aux = affinity.gt(.001).sum()  # mean *l*l / (l*(l+1)/2)  =  sum / numel * 2l/(l+1)
-            q_len = affinity.size(-1)
-            aux = aux * (2 * q_len / (q_len+1) / affinity.numel())
-        aux = aux.sub(affsm.detach()).add(affsm)
-        return torch.repeat_interleave(affinity,r,dim=1), aux
+        # return torch.repeat_interleave(affinity,r,dim=1)
+        return affinity
 
 
 
