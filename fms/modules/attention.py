8d12b5ac0caefcde0b51c90a9ee553949fa70193
Davis Wertheimer <dww78@cornell.edu>
2025-12-03 18:06:07 -0500

    Sanity check grad norm

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -151,8 +151,8 @@ class PassThresh(Function):
         ctx.dim1 = mask.size(3)
     @staticmethod
     def backward(ctx, g):
-        print(g)
         g = g / (g.numel() / g.size(0))
+        print(g)
         return g[:,None,None,None].expand(g.size(0), ctx.l, ctx.dim0, ctx.dim1)
 pass_thresh = PassThresh.apply
 
