195c0ad91f850aed7136e47a1fd1bf01082cde13
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-11 18:09:55 -0500

    Diag print

--- fms/models/llama.py
+++ fms/models/llama.py
@@ -579,7 +579,7 @@ class LLaMAHeadless(nn.Module):
             enc_out = g_t.view(b*n//128, 128, d)  # bn c d
             prior = dec.view(b*n//128, 128)[:,:1]  # bn 1
             pos = torch.arange(n//128, device=d_in.device)*128+128
-            print("GOTHERE", pos)
+            print("GOTHERE", list(range(n//128)))
             pos = pos.repeat(b)[:,None]  # bn 1
             (kv1, kv2) = past_key_value_states[-2], past_key_value_states[-1]
             # Rearrange caches into seq-batch of chunks
