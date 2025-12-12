76404d69d828b74a739b31fe09b3c22a65e5af72
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-12 14:52:54 -0500

    Fix print

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -957,7 +957,7 @@ class GatedMultiHeadAttention(nn.Module):
             if batch_size==2*32:
                 print(queries[0,0,0,:6], keys_compute[0,-2:,0,:6])
             else:
-                print(queries[0,4096,0,:6], keys_compute[0][[127,4096]][:,0,:6])
+                print(queries[0,4096,0,:6], keys_compute[0,127,0,:6], keys_compute[0,4096,0,:6])
 
         if attn_compute_dict["is_prefill"](**attn_kwargs):
             attn = attn_compute_dict["compute_prefill"](
