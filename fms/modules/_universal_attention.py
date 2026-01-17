dbecab7be3b305519ab44e5826bb917f6893aedb
spikerheado1234 <ahangupta.96@gmail.com>
2026-01-17 14:49:02 -0800

    final-inter: 4

--- fms/modules/_universal_attention.py
+++ fms/modules/_universal_attention.py
@@ -54,8 +54,8 @@ def _attn_fwd_inner(acc, l_i, m_i, q,  #
         qk = tl.dot(q, k)
         qk += aff
         ## Let's see if this custom masking fixes the issue. ##
-        mask = (offs_m[:, None] >= (start_n + offs_n[None, :])) & ((start_n + offs_n[None, :]) < N_CTX) & (offs_m[:, None] < N_CTX)
-        qk = qk + tl.where(mask, 0, -1.0e8)
+        #mask = (offs_m[:, None] >= (start_n + offs_n[None, :])) & ((start_n + offs_n[None, :]) < N_CTX) & (offs_m[:, None] < N_CTX)
+        #qk = qk + tl.where(mask, 0, -1.0e8)
         if STAGE == 2:
             m_ij = tl.maximum(m_i, tl.max(qk, 1))
             qk -= m_ij[:, None]
@@ -424,12 +424,12 @@ class _attention(torch.autograd.Function):
         desc_o = o
 
         ## Here, we launch an affinity matrix calculation kernel to simplify implementation. ##
-        #desc_affinity = _affinity_fwd(k, static_src, static_dest)
-        if not ac:
-            with torch.enable_grad():
-                desc_affinity = _gen_affinity_scores(k, static_src, static_dest)
-        else:
-            desc_affinity = _gen_affinity_scores(k, static_src, static_dest)
+        desc_affinity = _affinity_fwd(k, static_src, static_dest).contiguous()
+        #if not ac:
+        #    with torch.enable_grad():
+        #        desc_affinity = _gen_affinity_scores(k, static_src, static_dest)
+        #else:
+        #    desc_affinity = _gen_affinity_scores(k, static_src, static_dest)
 
         ## Specialize to this blk size for reasonable performance. ##
         BLOCK_M=128
@@ -515,10 +515,10 @@ class _attention(torch.autograd.Function):
         )
 
         ## Recompute affinity scores. ##
-        #affinity = _affinity_fwd(k, static_src, static_dest)
         if ctx.ac:
-            with torch.enable_grad():
-                affinity = _gen_affinity_scores(k, static_src, static_dest)
+            #with torch.enable_grad():
+            #    affinity = _gen_affinity_scores(k, static_src, static_dest)
+            affinity = _affinity_fwd(k, static_src, static_dest).contiguous()
 
         daffinity = torch.zeros(affinity.shape[0], Q_H * KV_H, N_CTX, N_CTX, dtype=affinity.dtype, device=affinity.device)
 
@@ -540,8 +540,8 @@ class _attention(torch.autograd.Function):
 
         daffinity = torch.reshape(daffinity, (daffinity.shape[0], Q_H, KV_H, daffinity.shape[2], daffinity.shape[3])).sum(1, keepdim=False)
 
-        #dk_new, dsrc, ddest = _affinity_bwd(k, static_src, static_dest, daffinity)
-        dk_new, dsrc, ddest = torch.autograd.grad(affinity, [k, static_src, static_dest], grad_outputs=daffinity)
+        dk_new, dsrc, ddest = _affinity_bwd(k, static_src, static_dest, daffinity)
+        #dk_new, dsrc, ddest = torch.autograd.grad(affinity, [k, static_src, static_dest], grad_outputs=daffinity)
         dk += dk_new
         return dq[:, :, :, :ctx.HEAD_DIM], dk[:,:,:,:ctx.HEAD_DIM], dv[:,:,:,:ctx.HEAD_DIM], None, None, dsrc, ddest, None, None
 
