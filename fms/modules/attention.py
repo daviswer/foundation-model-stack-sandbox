82187c183f071141160baa488b1082a721328d58
Davis Wertheimer <dww78@cornell.edu>
2026-02-07 20:10:57 -0500

    Update kernel and repeat heads

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -608,8 +608,8 @@ class MultiHeadAttention(nn.Module):
             torch.backends.cuda.enable_math_sdp(False)
             attn = F.scaled_dot_product_attention(
                 queries, 
-                torch.repeat_interleave(keys,r,dim=1), 
-                torch.repeat_interleave(values,r,dim=1), 
+                keys.repeat(1,r,1,1)
+                values.repeat(1,r,1,1)
                 attn_mask=mask,
                 scale=1,
             )  # b h l d
@@ -662,13 +662,21 @@ class MultiHeadAttention(nn.Module):
         else:
             return out, mask.view(batch_size, self.kvheads, -1, q_len, q_len)[:,:,0].detach().bfloat16()
 
+    # @torch.compile
+    # def _gen_affinity_scores(self, k, src, dest, r):
+    #     affinity = torch.einsum('bnqh, bnkh -> bnqk', k*src.sqrt().unsqueeze(-1), k*dest.sqrt().unsqueeze(-1)).relu().float().pow(2/3)
+    #     affinity = torch.log1p(affinity.clamp(min=0, max=1-1e-6).neg())
+    #     affinity = affinity.triu(1).cumsum(3).to(dtype=k.dtype)
+    #     affinity = affinity.masked_fill(torch.ones_like(affinity, dtype=torch.bool).tril(-1), -1e12).transpose(-1, -2)
+    #     return torch.repeat_interleave(affinity,r,dim=1)
+
     @torch.compile
     def _gen_affinity_scores(self, k, src, dest, r):
-        affinity = torch.einsum('bnqh, bnkh -> bnqk', k*src.sqrt().unsqueeze(-1), k*dest.sqrt().unsqueeze(-1)).relu().float().pow(2/3)
+        affinity = torch.einsum('bnqh, bnkh -> bnqk', k*dest.sqrt().unsqueeze(-1), k*src.sqrt().unsqueeze(-1)).relu().float().pow(2/3)
         affinity = torch.log1p(affinity.clamp(min=0, max=1-1e-6).neg())
-        affinity = affinity.triu(1).cumsum(3).to(dtype=k.dtype)
-        affinity = affinity.masked_fill(torch.ones_like(affinity, dtype=torch.bool).tril(-1), -1e12).transpose(-1, -2)
-        return torch.repeat_interleave(affinity,r,dim=1)
+        affinity = affinity.tril(-1).cumsum(2).to(dtype=k.dtype)
+        affinity = affinity.masked_fill(torch.ones_like(affinity, dtype=torch.bool).triu(1), float('-inf'))
+        return affinity.repeat(1,r,1,1)
 
 
 
